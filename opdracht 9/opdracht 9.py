f1713d7d74be32b284a4bbf680d479baee6e8c21
redninga <58817662+redninga@users.noreply.github.com>
2024-12-10 18:08:57 +0100

    attempt 1 for part 2

--- opdracht 9/opdracht 9.py
+++ opdracht 9/opdracht 9.py
@@ -41,4 +41,34 @@ for n,i in enumerate(c2):
     if i != -1:
         # print(i, n)
         total += i*n
+
+#%%
         
+content2 = [i for i in content]
+values = []
+for n,i in enumerate(content2):
+    if n % 2 == 0:
+        values.append(int(n/2))
+    else:
+        values.append(-1)
+
+values2 = copy.deepcopy(values)
+
+for n,i in enumerate(content2[::-1]):
+    if values[-(n+1)] != -1:
+        for m,j in enumerate(content2[:-(n+1)]):
+            if values[m] == -1:
+                if j >= i:
+                    values[m] = values[-(n+1)]
+                    values[-(n+1)] = -1# nog overige delen berekenen en dan
+
+c3 = []
+for n in range(len(values)):
+    for i in range(int(content2[n])):
+        c3.append(values[n])
+    
+total2 = np.int64(0)
+for n,i in enumerate(c3):
+    if i != -1:
+        # print(i, n)
+        total2 += i*n
