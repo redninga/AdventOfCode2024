2c19a8632a0856af2d8f202bc6a10ac2b9424bd5
redninga <58817662+redninga@users.noreply.github.com>
2024-12-11 15:03:35 +0100

    opdracht 10 part 1 and 2

--- opdracht 10/opdracht 10.py
+++ opdracht 10/opdracht 10.py
@@ -11,13 +11,54 @@ for n,row in enumerate(content):
     for m,i in enumerate(row):
         content2[n,m] = int(i)
 
-trailheadsvalues = np.zeros_like(2)
+content2 = np.pad(content2, [(1,1),(1,1)], mode="constant",constant_values= -1)
+
+trailheadsvalues = np.zeros_like(content2)
+trailheadsvalues2 = np.zeros_like(content2)
 
 def trail(x,y,array):
-    value = x
+    value = array[x,y]
+    if value == 9:
+        return [[x],[y]]
+    else:
+        x2 = []
+        y2 = []
+        if array[x,y+1] == value + 1:
+            result = trail(x,y+1,array)
+            # print(result)
+            x2.extend(result[0])
+            y2.extend(result[1])
+        if array[x,y-1] == value + 1:
+            result = trail(x,y-1,array)
+            # print(result)
+            x2.extend(result[0])
+            y2.extend(result[1])
+        if array[x+1,y] == value + 1:
+            result = trail(x+1,y,array)
+            # print(result)
+            x2.extend(result[0])
+            y2.extend(result[1])
+        if array[x-1,y] == value + 1:
+            result = trail(x-1,y,array)
+            # print(result)
+            x2.extend(result[0])
+            y2.extend(result[1])
+    return [x2,y2]
 
+# def unpacklists(nested):
+#     result = []
+#     for i in nested:
+#         if isinstance(i, list):
+#             result.
 
-for x in range(54):
-    for y in range(54):
+for x in range(len(content2)):
+    for y in range(len(content2[0])):
         if content2[x,y] == 0:
-            
+            result = trail(x,y,content2)
+            x2 = result[0]
+            y2 = result[1]
+            tops = list(zip(x2,y2))
+            trailheadsvalues[x,y] =  len(set(tops))
+            trailheadsvalues2[x,y] = len(x2)
+print(sum(sum(trailheadsvalues)))
+print(sum(sum(trailheadsvalues2)))
