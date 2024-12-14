6abd8220243d8a0b46d20c6b16a863899f3e7d2a
redninga <58817662+redninga@users.noreply.github.com>
2024-12-14 01:17:18 +0100

    Opdracht 13 deel 2 een stomme wiskundige had de laatste claw machine perongelijk uit mijn lijst gegooit :(

--- opdracht 13/opdracht 13.py
+++ opdracht 13/opdracht 13.py
@@ -7,7 +7,7 @@ import math
 #input = np.loadtxt("input.txt")
 file = open("input.txt","r")
 content = file.read()
-content = content.split("\n\n")[:-1]
+content = content.split("\n\n")
 total = 0
 for row in content:
     temp = row.split("\n")
@@ -27,24 +27,25 @@ for row in content:
     sy2 = sy-gaus1*sx
     b = round(sy2/yb2,0)
     a = round((sx - xb * b)/xa,0)
-    if a < 0 or a > 100 or b < 0 or b > 100:
-        print("Error too big or small")
-    elif a * xa + b * xb == sx and a * ya + b * yb == sy:
-        print(a,b)
+    # if a < 0 or a > 100 or b < 0 or b > 100:
+    #     print("Error too big or small")
+    if a * xa + b * xb == sx and a * ya + b * yb == sy:
+        # print(a,b)
         total += a * 3 + b
     else:
-        print("error",a, b)
+        # print("error",a, b)
+        pass
         
     
     a2 = (sx*yb-xb*sy)/(xa*yb-xb*ya)
     b2 = (xa*sy-sx*ya)/(xa*yb-xb*ya)
-    print(a2,b2)
+    # print(a2,b2)
 print(total)
 
 #%%
 add = np.int64(10000000000000)
 
-total = 0
+total = np.int64(0)
 for row in content:
     temp = row.split("\n")
     buta = temp[0]
@@ -63,15 +64,20 @@ for row in content:
     sy2 = sy-gaus1*sx
     b = round(sy2/yb2,0)
     a = round((sx - xb * b)/xa,0)
-    if a * xa + b * xb == sx and a * ya + b * yb == sy:
-        # print(a,b)
-        total += a * 3 + b
-    else:
-        pass
-        # print("error",a, b)
         
-    
     a2 = (sx*yb-xb*sy)/(xa*yb-xb*ya)
     b2 = (xa*sy-sx*ya)/(xa*yb-xb*ya)
-    print(a2,b2)
+    
+    if a * xa + b * xb == sx and a * ya + b * yb == sy:
+        print(a-a2,b-b2)
+        total += int(a) * 3 + int(b)
+        # if a < 0 or b < 0:
+        #     print(a,b)
+    else:
+        # print("error",(sx - xb * b)/xa, sy2/yb2)
+        pass
+        
+
+
+    # print(a2,b2)
 print(total)
